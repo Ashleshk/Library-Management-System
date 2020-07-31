9c479ed4e8a9226ae14f2d5d2f5d924c8adef503
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-07-31 21:14:21 +0530

    ViewBooks Function

---
+++ ViewBooks.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Ashlesh
+"""
+
+from tkinter import *
+from PIL import ImageTk,Image
+from tkinter import messagebox
+import pymysql
+
+# Add your own database name and password here to reflect in the code
+mypass = "root"
+mydatabase="rcpl_db"
+
+con = pymysql.connect(host="localhost",user="root",password=mypass,database=mydatabase)
+cur = con.cursor()
+
+# Enter Table Names here
+bookTable = "books" #Book Table
+    
+def View(): 
+    
+    root = Tk()
+    root.title("Library")
+    root.minsize(width=400,height=400)
+    root.geometry("600x500")
+
+
+    same=True
+    n=0.3
+
+    # Adding a background image
+    background_image =Image.open("library.jpg")
+    [imageSizeWidth, imageSizeHeight] = background_image.size
+    
+    newImageSizeWidth = int(imageSizeWidth*n)
+    if same:
+        newImageSizeHeight = int(imageSizeHeight*n) 
+    else:
+        newImageSizeHeight = int(imageSizeHeight/n)
+    
+    
+    
+    root.mainloop()
