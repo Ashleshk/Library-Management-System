5ece7008e97ca7cef8050351dd8d890d935cafa3
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-07-31 21:12:33 +0530

    Connecetion port Function

--- tset.py
+++ tset.py
@@ -3,3 +3,11 @@ import pymysql
 con = pymysql.connect('localhost', 'root', 
     'root', 'rcpl_db')
 
+with con:
+    
+    cur = con.cursor()
+    cur.execute("SELECT VERSION()")
+
+    version = cur.fetchone()
+    
+    print("Database version: {}".format(version[0]))
